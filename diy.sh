0b89be6ba0357afb2ef4084791bcae11fe85d9c5
sd745872 <62166675+sd745872@users.noreply.github.com>
2020-09-11 11:30:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.11/g' package/base-files/files/bin/config_generate
